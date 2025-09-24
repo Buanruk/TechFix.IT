7ae4153d5c03054d38e5fd6158584a1451404fc1
Buanruk <garfiled.peerapat@gmail.com>
2025-09-25 02:50:55 +0700

    1

--- db_connect.php
+++ db_connect.php
@@ -2,8 +2,8 @@
 // db_connect.php — เชื่อมต่อฐานข้อมูลครั้งเดียว แล้วให้ไฟล์อื่น include มาใช้
 
 $DB_HOST = "localhost";
-$DB_USER = "root";
-$DB_PASS = "254";
+$DB_USER = "phpadmin";
+$DB_PASS = "StrongPassword123!";
 $DB_NAME = "techfix";
 
 $conn = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
