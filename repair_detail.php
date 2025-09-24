31b2b02cb37967f9a3ed644dc00d774c1124d8c5
Buanruk <garfiled.peerapat@gmail.com>
2025-09-25 03:14:28 +0700

    1

--- repair_detail.php
+++ repair_detail.php
@@ -1,6 +1,6 @@
 <?php
 // ===== DB =====
-$conn = new mysqli("localhost", "phpadmin", "StrongPassword123!", "techfix");
+$conn = new mysqli("localhost", "root", "", "techfix");
 if ($conn->connect_error) { die("DB Error"); }
 $conn->set_charset("utf8");
 
--- update_status.php
+++ update_status.php
@@ -8,7 +8,7 @@ mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 $APP_BASE = '/techfix';
 
 // DB
-$conn = new mysqli("localhost", "phpadmin", "StrongPassword123!", "techfix");
+$conn = new mysqli("localhost", "root", "", "techfix");
 $conn->set_charset("utf8mb4");
 
 // รับเฉพาะ POST
